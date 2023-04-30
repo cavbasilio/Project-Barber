64f3878797aefb0b160cd7d4818c8e4d8038b12e
Jordan <86751624+cavbasilio@users.noreply.github.com>
2023-04-30 16:20:06 -0300

    Delete buscador-cursos-alura directory

--- buscador-cursos-alura/buscar-cursos.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-use GuzzleHttp\Client;
-
-$client = new Client();
-$resposta = $client->request('GET', 'https://cursos.alura.com.br/category/programacao/php');
-
-$html = $resposta->getBody();
